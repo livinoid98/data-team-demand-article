884d85ca8952bc1e3dabf2e063f89b7df5a7e11f
livinoid98 <ccy9803@naver.com>
2022-11-04 18:32:26 +0900

    connect db

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
+const mysql = require('mysql2/promise');
 const port = 3000;
+
+let connection = mysql.createPool({
+  host:'127.0.0.1',
+  port:3306,
+  user:'root',
+  password:'skxortn1!',
+  database: 'data_team'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -14,6 +23,12 @@ app.get('/', (req, res) => {
 
 app.get('/create', (req,res) => {
   res.render('create.ejs', {});
+});
+
+app.post('/create', (req,res) => {
+  let sql = `insert into checklist ("id", "title", "team", "name", "pageurl", "content", "deadline") values('test','성장운영본부 데이터팀','최찬영','https://cheremimaka.com/index.html','예시내용',NOW())`;
+  connection.query(sql);
+  res.redirect('/');
 });
 
 app.listen(port, () => {
