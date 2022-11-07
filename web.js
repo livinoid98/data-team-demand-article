60823d600309984dcd78710f0870ecc4a68d3510
livinoid98 <ccy9803@naver.com>
2022-11-07 13:46:09 +0900

    change port number

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const fs = require('fs');
-const port = 3000;
+const port = process.env.PORT; //3000;
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
